cefee74e2bc5db63a5206063a4e91ccfb4f89f0f
CarlosYes <161829023+CarlosYes@users.noreply.github.com>
2025-12-28 17:00:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+/**
+https://github.com/matteobruni/tsparticles
+https://particles.js.org
+https://confetti.js.org
+*/
+
+/**
+RangeValue = number | { min: number; max: number };
+*/
+
+fireworks(/*{
+    brightness: RangeValue;
+    colors: string | string[];
+    gravity: RangeValue;
+    minHeight: RangeValue;
+    rate: RangeValue;
+    saturation: RangeValue;
+    sounds: boolean;
+    speed: RangeValue;
+    splitCount: RangeValue;
+}*/);
